e9d3821cb302a52243061ba5c9bda64fe71ddf1c
Louis CLeriot <louis.cleriot@telecomnancy.net>
2023-11-22 18:48:21 -0500

    normalisation of the data with Robust Scaller

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,6 +3,11 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import numpy as np
+from scipy import stats
+from sklearn.preprocessing import RobustScaler
+
 
 
 @click.command()
@@ -14,6 +19,21 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    data = pd.read_csv(input_filepath+"/star_classification.csv")
+    #get rid of not use properties
+    data=data.drop(['obj_ID','run_ID','rerun_ID','cam_col','field_ID','spec_obj_ID','plate','MJD','fiber_ID'], axis=1)
+    #get rid of the outlier of u,g,z
+    data = data[data['g'] > -2000]
+    data = data[data['z'] > -2000]
+    data = data[data['u'] > -2000]
+    #scale the data
+    scaler = RobustScaler()
+    df_scaled = scaler.fit_transform(data.drop(['class'], axis=1))
+    df_scaled = pd.DataFrame(df_scaled, columns=data.columns[:-1])
+    df_scaled['class'] = data['class']
+    #save the data
+    df_scaled.to_csv(output_filepath+"/star_classification.csv", index=False)
+    
 
 
 if __name__ == '__main__':
