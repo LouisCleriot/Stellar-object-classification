8bbbafab1ae3c5e282d8c638a0695ad362f17f83
SERRAND Coralie <coralie.serrand@telecomnancy.eu>
2023-12-02 12:07:30 -0500

    fix hyperparam tuning and evaluation

--- src/models/classifier.py
+++ src/models/classifier.py
@@ -2,6 +2,7 @@ from joblib import dump, load
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.metrics import classification_report
 
 class Classifier :
     def __init__(self):
@@ -15,22 +16,18 @@ class Classifier :
     def predict(self,X):
         return self.model.predict(X)
 
-    def hyperparameter_tuning(self,X_train,y_train,parameters,search_type='grid',cv=5):
+    def hyperparameter_tuning(self,X_train,y_train,parameters,search_type='random',cv=5):
         if search_type == 'grid':
-            self.model = GridSearchCV(self.model,parameters,cv=cv)
+            clf = GridSearchCV(self.model,parameters,cv=cv)
         elif search_type == 'random':
-            self.model = RandomizedSearchCV(self.model,parameters,cv=cv)
-        self.train(X_train,y_train)
-        self.best_params = self.model.best_params_
-        self.model = self.model.best_estimator_
+            clf = RandomizedSearchCV(self.model,parameters,cv=cv,n_iter=10,random_state=0,n_jobs=-1,verbose=3,scoring='f1')
+        clf.fit(X_train,y_train)
+        self.best_params = clf.best_params_
+        self.model = clf.best_estimator_
 
-    def evaluate(self,X,y):
-        pred = self.predict(X)
-        accuracy = accuracy_score(y,pred)
-        precision = precision_score(y,pred,average='weighted')
-        recall = recall_score(y,pred,average='weighted')
-        f1 = f1_score(y,pred,average='weighted')
-        return {'accuracy': accuracy, 'precision': precision, 'recall': recall, 'f1': f1}
+    def evaluate(self,X_test,y_test):
+        report = classification_report(y_test,self.predict(X_test))
+        return report
 
     def save(self):
         dump(self.model, f'models/{self.name}.joblib')  
