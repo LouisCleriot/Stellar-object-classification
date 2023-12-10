64481ec93b9777539a5c15ce46ccbcc00a9d3ba4
SERRAND Coralie <coralie.serrand@telecomnancy.eu>
2023-12-09 22:27:58 -0500

    update KNN

--- src/models/KNNClassifier.py
+++ src/models/KNNClassifier.py
@@ -9,12 +9,3 @@ class KNNClassifier(Classifier):
         self.name = 'KNN'
         self.model = KNeighborsClassifier()
 
-    def hyperparameter_tuning(self, X_train, y_train):
-        parameters = {'n_neighbors': sp_randint(1, 40),
-              'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
-              'weights': ['uniform', 'distance'],
-              'p': sp_randint(1, 3),
-              'leaf_size': sp_randint(10, 100),
-              'n_jobs': [3]
-              }
-        return super().hyperparameter_tuning(X_train, y_train, parameters, search_type='random', cv=3, scoring='f1_macro', n_iteration=200)
